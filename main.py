101adec2f0dc4bbb6264eb8c69e54f9e3f5cd44e
PraneethSai <36663366+Praneethsai007@users.noreply.github.com>
2019-09-28 12:49:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,27 @@ from Multiply import Multiply
 from Division import Division
 from Subtraction import Subtraction
 
-a= Addition(2,3)
-s = Subtraction(2,3)
-d= Division(2,3)
-M = Multiply(2,3)    
-print("Addition is: ",a)
-print("Addition is: ", s)
-print("Addition is: ",d)
-print("Addition is: ",M)
+print("Enter the operation to perform")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Division")
+print("4. Multiplication")
+
+inp = input()
+
+if(inp=="1"):
+    op = Addition()
+elif(inp=="2"):
+    op = Subtraction()
+elif(inp=="3"):
+    op = Division()
+elif(inp=="4"):
+    op = Multiply()
+else:
+    print("Wrong input given")
+    sys.exit(0)
+
+    
+a = input("A:")
+b = input("B:")
+print(op.operation(a,b))
