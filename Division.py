ca9acd1199b95ea2f9db689db3ce0b03e81f9232
Arsh2106 <abdul.arshiya@st.niituniversity.in>
2019-09-28 12:20:51 +0530

    first

---
+++ Division.py
@@ -0,0 +1,7 @@
+class Division:
+    def __init__(self):
+        print("Division object created")
+        
+    def operation(self, a, b):
+        res = int(a)/int(b)
+        print("Division of a and b:"+str(res))
