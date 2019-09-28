2b3c18b8e96a6df93c8ad16a44872faedfa6f4f1
jashawanthreddy <v.jashwanthreddy@gmail.com>
2019-09-28 11:46:12 +0530

    subtraction class

---
+++ Subtraction.py
@@ -0,0 +1,7 @@
+class Subtraction:
+    def __init__(self):
+        print("Subtraction:")
+        
+    def operation(self, a, b):
+        res = int(a)-int(b)
+        print("Subtraction result:" + str(res))
