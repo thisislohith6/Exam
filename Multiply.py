272d7940996644788c5f4a7013662d1a79614370
Kowshikreddy007 <kowshikreddy.v@gmail.com>
2019-09-28 12:07:03 +0530

    Yo Yo..!!

---
+++ Multiply.py
@@ -0,0 +1,8 @@
+class Multiply:
+    def __init__(self):
+        print("Multiply object created")
+
+    def operation(self, a, b):
+        res = int(a)*int(b)
+        print("Multiplication of a and b:"+str(res))
+        
