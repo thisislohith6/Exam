ffb8ea15bad4ffc7806aef69f96ab3d5eb692fbc
PraneethSai <36663366+Praneethsai007@users.noreply.github.com>
2019-09-28 12:46:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from Multiply import Multiply
 from Division import Division
 from Subtraction import Subtraction
 
-int a= Addition(2,3)
-int s = Subtraction(2,3)
-int d= Division(2,3)
-int M = Multiply(2,3)    
+a= Addition(2,3)
+s = Subtraction(2,3)
+d= Division(2,3)
+M = Multiply(2,3)    
 print("Addition is: " a)
 print("Addition is: " s)
 print("Addition is: " d)
